1904165d3c6339e2c63c650e935d4876f4b6b84c
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-05-28 22:20:02 +0300

    CounterRunnable sınıfı içerisinde Thread uyutma ve durdurma ile ilgili örnekleme yapıldı.

--- src/threadsAndConcurrency/CounterRunnable.java
+++ src/threadsAndConcurrency/CounterRunnable.java
@@ -1,18 +1,31 @@
 package threadsAndConcurrency;
 
-public class CounterRunnable implements Runnable{
+public class CounterRunnable implements Runnable {
     private String name;
+    // Thread Bekletmek(sleep)
+    private int id;
+    // Thread Durdurmak için olan kısım
+    private boolean isRun = true;
 
-    public CounterRunnable(String name) {
+    public CounterRunnable(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 
     @Override
     public void run() {
         System.out.println(this.getName() + " sayacı başladı");
-        for (int i = 0; i<=100; i++){
-            System.out.println(this.getName() + " : " + i);
-        }
+        // durdurma kısmı
+        int i = 0; // durdurma kısmı için for döngüsü engel oluyor kaldıracağız
+        while (isRun){
+                // her artış kadar Thread i uyutucak(bekletecek)
+                try {
+                    Thread.sleep(1000L * this.id); // Kod buraya geldiğinde 1'er saniye bekliyor ve devam ediyor
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(this.getName() + " : " + i++);
+            }
     }
 
     public String getName() {
@@ -21,5 +34,9 @@ public class CounterRunnable implements Runnable{
 
     public void setName(String name) {
         this.name = name;
+    }
+    // kendimiz Stop methodu yazabiliriz Thread için
+    public void stop(){
+        this.isRun = false;
     }
 }
--- src/threadsAndConcurrency/Main.java
+++ src/threadsAndConcurrency/Main.java
@@ -1,19 +1,34 @@
 package threadsAndConcurrency;
 
 public class Main {
-    public static void main(String[] args) {
-        CounterRunnable c1 = new CounterRunnable("c1");
-        CounterRunnable c2 = new CounterRunnable("c2");
-        CounterRunnable c3 = new CounterRunnable("c3");
+    public static void main(String[] args) throws InterruptedException {
+        // ##### Thread Durdurmak ve Bekletmek #####
 
-        // Thread sınıfı açıp Runnable ataması yapabiliyoruz
+        // Thread durdurmak için burda CounterRunnable için bi obje oluşturmak gerekece
+        CounterRunnable c1 = new CounterRunnable("c1",1);
         Thread t1 = new Thread(c1);
-        Thread t2 = new Thread(c2);
-        Thread t3 = new Thread(c3);
-
         t1.start();
-        t2.start();
-        t3.start();
+        Thread.sleep(3000);// burda aslında 3 saniye sonra c1 objesinin Thread(iş parçacığını) durdumayı hedefledik
+        c1.stop(); // bu şekilde manual olarak 3 saniye sonra durdurmuş olduk Thread'i
+
+//        CounterRunnable c2 = new CounterRunnable("c2",2);
+//        Thread t2 = new Thread(c2);
+//        t2.start();
+//        Thread t2 = new Thread(new CounterRunnable("c2",2));
+//        t2.start();
+
+//        CounterRunnable c1 = new CounterRunnable("c1", id);
+//        CounterRunnable c2 = new CounterRunnable("c2", id);
+//        CounterRunnable c3 = new CounterRunnable("c3", id);
+//
+//        // Thread sınıfı açıp Runnable ataması yapabiliyoruz
+//        Thread t1 = new Thread(c1);
+//        Thread t2 = new Thread(c2);
+//        Thread t3 = new Thread(c3);
+//
+//        t1.start();
+//        t2.start();
+//        t3.start();
 //        Counter c1 = new Counter();
 //
 //        Counter c2 = new Counter();
